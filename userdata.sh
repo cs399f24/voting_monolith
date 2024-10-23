27caaf7477825846bc3374b6a5bac928ab31db2d
Ben Coleman <colemanb@moravian.edu>
2024-10-23 10:24:27 -0400

    enable redis

--- userdata.sh
+++ userdata.sh
@@ -5,6 +5,7 @@ cd voting_monolith
 python3 -m venv .venv
 .venv/bin/pip install -r requirements.txt
 yum install -y redis6
+systemctl enable redis6
 systemctl start redis6
 echo "REDIS_HOST=localhost" >> .env
 echo "REDIS_PORT=6379" >> .env
